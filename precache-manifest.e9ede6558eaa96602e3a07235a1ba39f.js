bf4a57b1488d3ff026be1d4ac5e112d81ce5532f
Hicham Benjelloun <hbenjelloun@pm.me>
2020-02-16 23:20:56 +0100

    Updates

---
+++ precache-manifest.e9ede6558eaa96602e3a07235a1ba39f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a142a274ff7a8875e3ab00a99bf33d2",
+    "url": "/river-todoapp/index.html"
+  },
+  {
+    "revision": "3f5fe942c55503cbccc9",
+    "url": "/river-todoapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "403c938493ab3511d29a",
+    "url": "/river-todoapp/static/js/2.05252679.chunk.js"
+  },
+  {
+    "revision": "f9d2326e9f9956f1bf6a1594b879b02b",
+    "url": "/river-todoapp/static/js/2.05252679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f5fe942c55503cbccc9",
+    "url": "/river-todoapp/static/js/main.9d2980b7.chunk.js"
+  },
+  {
+    "revision": "8741078765ebaf230436",
+    "url": "/river-todoapp/static/js/runtime-main.45cb1725.js"
+  }
+]);
